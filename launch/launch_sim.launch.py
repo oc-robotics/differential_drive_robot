0c986196a64505f979a165344e99181c4845b661
Ye Chan Lin <yechanlin15703@gmail.com>
2025-03-02 22:26:59 -0800

    enabled gazebo robot movement with updated plugin

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,47 +1,30 @@
 import os
- 
+
 from ament_index_python.packages import get_package_share_directory
- 
- 
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
- 
+
 from launch_ros.actions import Node
- 
- 
- 
+
 def generate_launch_description():
- 
- 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
- 
-    package_name='differential_drive_robot' #<--- CHANGE ME
- 
+    package_name='differential_drive_robot'
+
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
- 
-    # Create launch argument for the world
-
-#    default_world = os.path.join(
- #       get_package_share_directory(package_name),
-  #      'worlds',
-   #     'empty.world'
-    #)
 
     default_world = '/usr/share/gz/gz-sim7/worlds/empty.sdf'
-
     world = LaunchConfiguration('world')
 
     world_arg = DeclareLaunchArgument(
         'world',
-        default_value = default_world,
-        description = 'World to load'
+        default_value=default_world,
+        description='World to load'
     )
 
     # Include the Gazebo launch file, provided by the ros_gz_sim package
@@ -50,19 +33,29 @@ def generate_launch_description():
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
                     launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
- 
-    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+
+    # Run the spawner node from the ros_gz_sim package
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot'],
+                                   '-name', 'my_bot',
+                                   '-z', '0.1'],
                         output='screen')
- 
- 
- 
+
+    # Bridge for cmd_vel and other topics
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': os.path.join(get_package_share_directory(package_name), 'config', 'bridge.yaml'),
+        }],
+        output='screen'
+    )
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         world_arg,
         gazebo,
         spawn_entity,
+        bridge,
     ])
