bd9336931d34cd0bc028686a2f473ed1797ffc9e
chxtio <chxtio@gmail.com>
2025-04-06 01:59:47 -0700

    Add controller parameters file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -21,7 +21,8 @@ def generate_launch_description():
     default_world = os.path.join(
 	get_package_share_directory(package_name),
 	'worlds',
-	'obstacle_world.sdf'
+	# 'obstacle_world.sdf'
+    'empty.world'
     )
 
     world = LaunchConfiguration('world')
